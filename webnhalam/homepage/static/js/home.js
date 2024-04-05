dda749e2c55b0599b2129652e9c4b41e1357ff9a
Saroo-Coding <congdanh785@gmail.com>
2024-04-05 23:10:36 +0700

    deploynow

--- webnhalam/homepage/static/js/home.js
+++ webnhalam/homepage/static/js/home.js
@@ -50,14 +50,9 @@ modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
 
-
-// custom select variables
-const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
-select.addEventListener("click", function () { elementToggleFunc(this); });
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
